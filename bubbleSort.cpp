cf6d8a7fc34821f739ad43231634b10f865bebf5
galva041 <galva041>
2020-11-19 08:56:12 -0800

    bubbleSort fixed

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,16 +1,12 @@
 #include "bubbleSort.hpp"
 
-void BubbleSort::Sort(Container* container) { 
-    int flag = 1;   
-    //Base* temp;             
+void BubbleSort::sort(Container* container) {             
     int size = container->size(); 
-    for(int i = 1; (i <= size) && flag; i++) {
-        flag = 0;
-        for (int j = 0; j < (size - 1); j++) {
+    for(int i = 0; i < size-1; i++) {
+        for (int j = 0; j < size -i-1; j++) {
             if (container->at(j+1)->evaluate() < container->at(j)->evaluate()) { 
-                container->swap(j+1, j);      
+                container->swap(j, j+1);      
             }
         }
     }
-    return;  
 } 
